c13ef11e9959a2edd8d05f014d0d7bb856bacbac
ee35a1f6-8cd3-47fb-8b40-bbebce819a42 <snopy324@gmail.com>
2021-05-13 21:55:54 +0800

    write chessboard 優化

--- Queens/Program.cs
+++ Queens/Program.cs
@@ -34,7 +34,7 @@ namespace Queens
 
             stopwatch.Stop();
 
-            //Console.WriteLine(stringBuilder.ToString());
+            Console.WriteLine(stringBuilder.ToString());
 
             Console.WriteLine(successed);
 
@@ -87,16 +87,13 @@ namespace Queens
             {
                 for (int y = 0; y < dimansion; y++)
                 {
-                    //stringBuilder.Append($"[{x},{y}] ");
-                    stringBuilder.Append(Convert.ToInt32(chessBoard[x, y]));
-                    //stringBuilder.Append($"     ");
+                    stringBuilder.Append(chessBoard[x, y] ? 'Q' : '.');
                 }
 
                 stringBuilder.AppendLine();
             }
 
-            stringBuilder.AppendLine();
-            stringBuilder.AppendLine();
+            stringBuilder.AppendLine().AppendLine();
         }
 
         static void setLayer(int x, int y, bool placeQueen)
@@ -104,105 +101,38 @@ namespace Queens
             setYLayer(x, y, placeQueen);
             setSlashLayer(x, y, placeQueen);
             setBackslashLayer(x, y, placeQueen);
-        }
-        static void setYLayer(int x, int y, bool placeQueen)
-        {
-            yLayer[x] = placeQueen;
-        }
-        static void setSlashLayer(int x, int y, bool placeQueen)
-        {
-            slashLayer[dimansion - 1 - x + y] = placeQueen;
-        }
-        static void setBackslashLayer(int x, int y, bool placeQueen)
-        {
-            backslashLayer[x + y] = placeQueen;
+
+            static void setYLayer(int x, int y, bool placeQueen)
+            {
+                yLayer[x] = placeQueen;
+            }
+            static void setSlashLayer(int x, int y, bool placeQueen)
+            {
+                slashLayer[dimansion - 1 - x + y] = placeQueen;
+            }
+            static void setBackslashLayer(int x, int y, bool placeQueen)
+            {
+                backslashLayer[x + y] = placeQueen;
+            }
         }
 
         static bool check(int x, int y)
         {
             return checkLeft(x, y) || checkLeftTop(x, y) || checkLeftDown(x, y);
+            static bool checkLeftTop(int x, int y)
+            {
+                return backslashLayer[x + y];
+            }
+            static bool checkLeft(int x, int y)
+            {
+                return yLayer[x];
+            }
+            static bool checkLeftDown(int x, int y)
+            {
+                return slashLayer[dimansion - 1 - x + y];
+            }
         }
-        static bool checkLeftTop(int x, int y)
-        {
-            return backslashLayer[x + y];
 
-            //for (int i = 0; i <= Math.Min(x, y); i++)
-            //{
-            //    if (chessBoard[x - i, y - i])
-            //    {
-            //        return true;
-            //    }
-            //}
-
-            //return false;
-
-            //return Enumerable.Range(1, Math.Min(x, y)).Select(i => board[x - i, y - i]).Any(b => b);
-
-            //var a = Enumerable.Range(1, Math.Min(x, y)).Select(i => new BoardInfo
-            //{
-            //    X = x - i,
-            //    Y = y - i,
-            //    Queen = board[x - i, y - i]
-            //});
-            //return a.Any(b => b.Queen);
-        }
-        static bool checkLeft(int x, int y)
-        {
-            return yLayer[x];
-
-            //for (int i = 0; i <= y; i++)
-            //{
-            //    if (chessBoard[x, y - i])
-            //    {
-            //        return true;
-            //    }
-            //}
-
-            //return false;
-
-            //return Enumerable.Range(1, y).Select(i => board[x, y - i]).Any(b => b);
-
-            //var a = Enumerable.Range(1, y).Select(i =>
-            // new BoardInfo
-            // {
-            //     X = x,
-            //     Y = y - i,
-            //     Queen = board[x, y - i]
-            // });
-            //return a.Any(b => b.Queen);
-        }
-        static bool checkLeftDown(int x, int y)
-        {
-            return slashLayer[dimansion - 1 - x + y];
-
-            //for (int i = 0; i <= Math.Min((dimansion - 1) - x, y); i++)
-            //{
-            //    if (chessBoard[x + i, y - i])
-            //    {
-            //        return true;
-            //    }
-            //}
-
-            //return false;
-
-            //return Enumerable.Range(1, Math.Min((dimansion - 1) - x, y)).Select(i => board[x + i, y - i]).Any(b => b);
-
-            //var a = Enumerable.Range(1, Math.Min((dimansion - 1) - x, y)).Select(i =>
-            //    new BoardInfo
-            //    {
-            //        X = x + i,
-            //        Y = y - i,
-            //        Queen = board[x + i, y - i]
-            //    });
-            //return a.Any(b => b.Queen);
-        }
-    }
-
-    public class BoardInfo
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public bool Queen { get; set; }
     }
 }
 
